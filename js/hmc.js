93a25f18f19a68ce28798693f37f1a6cfc3720ef
cuonghl1028 <cuonghamanhcuong12@gmail.com>
2020-05-01 20:01:28 +0700

    jhcxb

--- js/hmc.js
+++ js/hmc.js
@@ -14,7 +14,7 @@ var image = [
             else{
                 i = 0;
             }
-        setTimeout(changeImage,1000);
+        setTimeout(changeImage,2000);
         }
 window.onload = changeImage;
 var mainIcon = document.querySelectorAll('.main__part-icon');
@@ -29,12 +29,15 @@ function moveheader(){
     var value = window.scrollY;
     if(value>0 && value<500)
     {
-        header.style.top = '-25px';
+        header.style.top = '-175px';
        
     }
-    else{
-        header.style.top = 0;   
+    else if(value>500){
+        header.style.top = 'calc((var(--header-head) + var(--header-menu)) - var(--header))';   
         header.style.transition = '0.3s';
+    }
+    else{
+        header.style.top = 0;
     }
 };
 var btnReload = document.getElementById('idbtnreload')
@@ -53,3 +56,12 @@ btnReload.addEventListener('click',function backTop(){
     var scrolled = window.scrollTo;
     scrolled(0,0);
 })
+var headerLogin = document.querySelector('.header__head-login-text');
+var modal = document.querySelector('.modal')
+headerLogin.addEventListener('click',() =>{
+    modal.style.display = 'flex';
+});
+var modalOverlay = document.querySelector('.modal__overlay');
+modalOverlay.addEventListener('click',() =>{
+    modal.style.display = '';
+});
